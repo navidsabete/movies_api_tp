59ecdfa7b1fa6a6204b76c8bc4e83674b9c61460
HoniTahina <ricatahina@gmail.com>
2025-12-05 09:50:20 +0100

    templates

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def temp():
+    return render_template("home.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
